8b092ba8d45b73e0515bfdf2a25b60a1906d8add
Treya <Cameron@St-Amant-SP3>
2017-01-26 14:04:06 -0700

    1/26/17 - Commit to repository.

--- ClueTeam3/src/clueteam3/ClueTeam3.java
+++ ClueTeam3/src/clueteam3/ClueTeam3.java
@@ -5,7 +5,10 @@
  */
 package clueteam3;
 
+import byui.cit260.clueTeam3.model.MansionMap;
 import byui.cit260.clueTeam3.model.Player;
+import byui.cit260.clueTeam3.model.Room;
+import byui.cit260.clueTeam3.model.Weapon;
 
 /**
  *
@@ -22,9 +25,34 @@ public class ClueTeam3 {
         playerOne.setName("Fred Flinstone");
         playerOne.setBestTime(7.00);
     
-        
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        
+        MansionMap mapOne = new MansionMap();
+        
+        mapOne.setColumnCount(2);
+        mapOne.setRowCount(5);
+        
+        String mansionMapInfo = mapOne.toString();
+        System.out.println(mansionMapInfo);
+        
+        Room roomOne = new Room();
+        
+        roomOne.setName("Library");
+        roomOne.setDescription("The room with the books");
+        roomOne.setAttribute(5);
+        
+        String roomInfo = roomOne.toString();
+        System.out.println(roomInfo);
+        
+        Weapon weaponOne = new Weapon();
+        
+        weaponOne.setDescription("Sharp, be careful not to cut yourself");
+        weaponOne.setLocation("Library");
+        weaponOne.setWeaponType("Dagger");
+        
+        String weaponInfo = weaponOne.toString();
+        System.out.println(weaponInfo);
     }
     
 }
