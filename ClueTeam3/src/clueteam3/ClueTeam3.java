0711adaaf832f7b1b6f87a95c41890506d44c426
catrinalonghurst1 <catrinalonghurst1@catrinas-imac.home>
2017-01-26 13:23:01 -0500

    1/26/17 Added Game, Character and Time

--- ClueTeam3/src/clueteam3/ClueTeam3.java
+++ ClueTeam3/src/clueteam3/ClueTeam3.java
@@ -5,7 +5,10 @@
  */
 package clueteam3;
 
+import byui.cit260.clueTeam3.model.Game;
 import byui.cit260.clueTeam3.model.Player;
+import byui.cit260.clueTeam3.model.Character;
+import byui.cit260.clueTeam3.model.Time;
 
 /**
  *
@@ -25,6 +28,37 @@ public class ClueTeam3 {
         
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
-    }
-    
+        
+        Game gameOne = new Game();
+        
+        gameOne.setTotalTime(12.0);
+        gameOne.setNoPeople(0);
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo); 
+        
+        
+        Character characterOne = new Character();
+        
+        characterOne.setName("name");
+        characterOne.setDescription("description");
+        characterOne.setCoordinates(0);
+        characterOne.setType("type");
+        
+        String characterInfo = characterOne.toString();
+        System.out.println(characterInfo);
+        
+        
+        Time timer = new Time();
+        
+        timer.setRun("countdown");
+        timer.setCountdownTime(0);
+        timer.setDisplayTime(0);
+        
+        String timeInfo = timer.toString();
+        System.out.println(timeInfo);
+                
+                
+     
+}
 }
