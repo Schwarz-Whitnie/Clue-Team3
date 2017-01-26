42cf086fe3cc8fa18f2bee5139e9335d9f9c71a9
Treya <>
2017-01-26 10:51:50 -0700

    1/26/17 - Commit to repository. Classes - MansionMap, Room, and Weapon

---
+++ ClueTeam3/src/byui/cit260/clueTeam3/model/Weapon.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.clueTeam3.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Cameron
+ */
+public class Weapon implements Serializable {
+    
+    private String description;
+    private String location;
+    private String weaponType;
+
+    public Weapon() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(String weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Objects.hashCode(this.location);
+        hash = 79 * hash + Objects.hashCode(this.weaponType);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Weapon other = (Weapon) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.location, other.location)) {
+            return false;
+        }
+        if (!Objects.equals(this.weaponType, other.weaponType)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{" + "description=" + description + ", location=" + location + ", weaponType=" + weaponType + '}';
+    }
+}
