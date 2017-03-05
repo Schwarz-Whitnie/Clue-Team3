69c7123ddcd9c4bc409e8abcb88a38936a8c1a18
whitnieschwarz <whitnieschwarz@whitniesair1297>
2017-03-04 22:22:06 -0700

    Merge origin/master
    Conflicts:
    	ClueTeam3/src/clueteam3/ClueTeam3.java

--- ClueTeam3/src/byui/cit260/clueTeam3/view/TimeRemainingView.java
+++ ClueTeam3/src/byui/cit260/clueTeam3/view/TimeRemainingView.java
@@ -12,142 +12,42 @@ import java.util.Scanner;
  *
  * @author whitnieschwarz
  */
-public class TimeRemainingView {
+public class TimeRemainingView extends View {
     
-    private String roomPrompt;
-    private final String locationPrompt;
-    private final String passagewayPrompt;
     private double timeRemaining;
     
     public TimeRemainingView() {
-        this.roomPrompt = "\nPlease enter number of rooms passed through:";
+        super ("\nPlease enter number of rooms passed through:");
         //display the locations prompt
-        this.locationPrompt = "\nPlease enter number of locations passed through:";
-        this.passagewayPrompt = "\nPlease enter number of passageways used:";
+        super.displayMessage = "\nPlease enter number of locations passed through:";  
+        super.displayMessage = "\nPlease enter number of passageways used:";
         
     }
     
-    void displayRoomPrompt() {
-        boolean done = false; // user flag to not done
-        do { 
-            // prompt for and get number of rooms 
-            String roomsTraveled = this.getRoomsTraveled();
-            if (roomsTraveled.toUpperCase().equals("Q")) // user wants to quit
-                return; // exit the game 
-            
-            String locationsTraveled = this.getLocationsTraveled();
-            if (locationsTraveled.toUpperCase().equals("Q")) // user wants to quit
-                return; // exit the game 
-            
-            String passagewaysUsed = this.getPassagewaysUsed();
-            if (passagewaysUsed.toUpperCase().equals("Q")) // user wants to quit
-                return; // exit the game 
-            
-            
-            // do the request action and display the next view 
-            done = this.doAction(roomsTraveled, locationsTraveled, passagewaysUsed); 
-           
-        }while (!done);
+    public void locationPrompt() {
         
-    }
-private String getRoomsTraveled() {
-        
-        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
-        String value = ""; // value to be returned
-        boolean valid = false; //initialize to not valid
-        
-        while (!valid) { // loop while an invalid value is enter
-            System.out.println("\n" + this.roomPrompt);
-            
-            value = keyboard.nextLine(); // get next line typed one keyboard
-            value = value.trim(); // trim off leading and trailing blanks
-            
-            if (value.length() < 1) { // value is blank
-                System.out.println("\nInvalid value: value can not be blank");
-                continue;
-            }
-            
-            break; // end the loop
-        }
-        return value; // return the value entered
+        System.out.println("\nPlease enter number of locations passed through:");
     }
     
-private String getLocationsTraveled() {
+    public void passagewayPrompt() {
         
-        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
-        String value = ""; // value to be returned
-        boolean valid = false; //initialize to not valid
-        
-        while (!valid) { // loop while an invalid value is enter
-            System.out.println("\n" + this.locationPrompt);
-            
-            value = keyboard.nextLine(); // get next line typed one keyboard
-            value = value.trim(); // trim off leading and trailing blanks
-            
-            if (value.length() < 1) { // value is blank
-                System.out.println("\nInvalid value: value can not be blank");
-                continue;
-            }
-            
-            break; // end the loop
-        }
-        return value; // return the value entered
+        System.out.println("\nPlease enter number of passageways used:");
     }
 
-private String getPassagewaysUsed() {
+@Override 
+   public boolean doAction(String value) {
         
-        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
-        String value = ""; // value to be returned
-        boolean valid = false; //initialize to not valid
-        
-        while (!valid) { // loop while an invalid value is enter
-            System.out.println("\n" + this.passagewayPrompt);
-            
-            value = keyboard.nextLine(); // get next line typed one keyboard
-            value = value.trim(); // trim off leading and trailing blanks
-            
-            if (value.length() < 1) { // value is blank
-                System.out.println("\nInvalid value: value can not be blank");
-                continue;
-            }
-            
-            break; // end the loop
-        }
-        return value; // return the value entered
-    }
-
-   public boolean doAction(String roomsTraveled, String locationsTraveled, String passagewaysUsed) {
-        
-       if (roomsTraveled.equals("-1")) {
+       if (value.equals("-1")) {
            System.out.println("\nInvalid number of rooms: "
                    + "The number of rooms must be greater than 0");
            return false;
        }
        
-       if (locationsTraveled.equals("-1")) {
-           System.out.println("\nInvalid number of locations: "
-                   + "The number of locations must be greater than 0");
-           return false;
-       }
-       
-       if (passagewaysUsed.equals("-1")) {
-           System.out.println("\nInvalid number of passageway used: "
-                   + "The number of passageways must be between 0-3");
-           return false;
-       }
-       
-//       passagewaysUsed = " ";
-       double d = Double.parseDouble(passagewaysUsed);
-       
-//       roomsTraveled = " ";
-       double r = Double.parseDouble(roomsTraveled);
-       
-//       locationsTraveled = " ";
-       double l = Double.parseDouble(locationsTraveled);
+        double r = Double.parseDouble(value);
        
        //call calcTotalTime() control function
        TimeControl timeControl = new TimeControl();
-       timeRemaining = TimeControl.calcTotalTime(r, l, d);
+       timeRemaining = TimeControl.calcTotalTime(r,r,r);
        
        //display next view
        this.displayNextView(timeControl);
@@ -166,6 +66,7 @@ private String getPassagewaysUsed() {
 
 
 }
+
 
         
 
