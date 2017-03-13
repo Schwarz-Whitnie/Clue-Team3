4775cb5d1950ea2687f57fdb56ef70ab3113d75e
Longhurst-Catrina <catrinalonghurst1@catrinas-imac.home>
2017-03-13 09:55:22 -0400

    3/3/17 Detective Notebook Progress

--- ClueTeam3/src/byui/cit260/clueTeam3/view/PoisonControlView.java
+++ ClueTeam3/src/byui/cit260/clueTeam3/view/PoisonControlView.java
@@ -4,50 +4,89 @@
  * and open the template in the editor.
  */
 package byui.cit260.clueTeam3.view;
+import byui.cit260.clueTeam3.control.CharacterControl;
+import java.util.Scanner;
 
 /**
  *
  * @author catrinalonghurst1
  */
-public class PoisonControlView extends View{
+
+public class PoisonControlView {
       
-    private final String poisonControl;
+    private String poisonPrompt;
+    private String poisonControl;
 
     
     public PoisonControlView() {
         
         //displays the posion prompt
-        super("\nPlease enter the amount of Poison missing from "
-                + "the vile.");
+        this.poisonPrompt = "\nPlease enter the amount of Poison missing from "
+                + "the vile.";
         
     }
+   
     
-    
-    @Override
-    public boolean doAction(String amtPoison) {
+    void displayPoisonPrompt() {
+        boolean done = false;
+        do{
+            String amtPoison = this.getAmountPoison();
+            if (amtPoison.toUpperCase().equals("Q")) // user wants to quit
+                return; //exit the game
+            
+        done = this.doAction(amtPoison);  
         
-        if (amtPoison != 69) {
-        System.out.println ("\nInvalid entry. Please try again." );
-        return false;
+        } while (!done);
     }
-        if (amtPoison == 69) 
-        return true;
+    
+    private String getAmountPoison() {
+        Scanner keyboard = new Scanner(System.in); 
+        String value = ""; //value to be returned
+        boolean valid = false;
+        
+        while (!valid) {
+            System.out.println("\n" + this.poisonPrompt);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length() < 0) { // value is blank
+                System.out.println("\n Value cannot be blank");
+                continue;
+            }
+            
+            break; //end loop
+        }
+        return value; //return the value entered. 
+    }
+
+   public boolean doAction(String amtPoison) {
+        
+       if (amtPoison.equals("68")) {
+           System.out.println("\nInvalid entry. Try again");
+           return false;
+       } 
+        
+       double p = Double.parseDouble(amtPoison);
+       
+       CharacterControl characterControl = new CharacterControl();
 
        
-  
-    }
-
-    public double doAction(double amtPoison) 
-    
-    {  
-        double poisonPerMinute = 0.05;
-        double timePoison = amtPoison / (poisonPerMinute*60);
-        timePoison = Math.round (timePoison *10) / 10;
-        return timePoison;
+       this.displayNextView(characterControl);
+       return true;
+   }
+   
+   private void displayNextView(CharacterControl characterControl) {
+        String timePoison = null;
+       
+       System.out.println("\nThe poison was administered" + characterControl 
+               + "Good work!");
+            
+   
     }
 
         
     }
 
 
-    
+
