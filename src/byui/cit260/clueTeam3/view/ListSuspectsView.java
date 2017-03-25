3efefc996ef639361a44a60ae3536bf5eda3c8e9
Treya <camer@St-Amants>
2017-03-25 10:17:51 -0600

    3-23-17 treya edits, working on ListWeaponsView

--- src/byui/cit260/clueTeam3/view/ListSuspectsView.java
+++ src/byui/cit260/clueTeam3/view/ListSuspectsView.java
@@ -5,13 +5,74 @@
  */
 package byui.cit260.clueTeam3.view;
 
+import byui.cit260.clueTeam3.exeptions.GameControlException;
+import byui.cit260.clueTeam3.exeptions.ListSuspectsViewException;
+import byui.cit260.clueTeam3.model.Clue;
+import byui.cit260.clueTeam3.model.DetectiveNotebook;
+import byui.cit260.clueTeam3.model.Game;
+import byui.cit260.clueTeam3.model.Suspects;
+import clueteam3.ClueTeam3;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.Objects;
 import java.util.Scanner;
+
+
 
 /**
  *
  * @author catrinalonghurst1
  */
 public class ListSuspectsView extends View {
+
+    static void getSortedListSuspectsView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String description;
+    private boolean accused;
+    private String suspects;
+    
+    private static PrintWriter outfile = null;
+    private static BufferedReader infile = null;
+    
+    private static PrintWriter logFile = null;
+
+    public static PrintWriter getLogFile() {
+        return logFile;
+    }
+
+    public static void setLogFile(PrintWriter logFile) {
+        ListSuspectsView.logFile = logFile;
+    }
+
+    public String getSuspects() {
+        return suspects;
+    }
+
+    public static PrintWriter getOutfile() {
+        return outfile;
+    }
+
+    public static BufferedReader getInfile() {
+        return infile;
+    }
+
+    public String getDisplayMessage() {
+        return displayMessage;
+    }
+
+    public BufferedReader getKeyboard() {
+        return keyboard;
+    }
+
+    public PrintWriter getConsole() {
+        return console;
+    }
+    
     
     public ListSuspectsView() {
         
@@ -30,7 +91,7 @@ public class ListSuspectsView extends View {
             
     }
      
-    @Override
+
     public boolean doAction(String value) {
         
         value = value.toUpperCase(); //convert choice to upper case
@@ -61,7 +122,85 @@ public class ListSuspectsView extends View {
         
          return false;
     }
+ 
+    
+    public ListSuspectsView (String description, boolean accused) {
+        this.description = description;
+        this.accused = accused;
+}
 
+    /* public DetectiveNotebook(int rowCount, int columnCount) {
+        
+        if(rowCount<1 || columnCount <1) {
+            System.out.println("The number of rows and columns must be bigger than 1");
+            return;
+        }
+
+        
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+        
+        this.notebook = new DetectiveNotebook[rowCount][columnCount];
+        for (int row=0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
+                //create and initialize new clue object instance
+                Clue clues = new Clue();
+            }
+        }
+
+} */
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+    this.description = description;
+}
+
+    public boolean isAccused() {
+        return accused;
+    }
+
+    public void setAccused(boolean accused) {
+        this.accused = accused;
+    }
+    
+
+    @Override
+    public String toString() {
+    return "ListSuspectsView" + "description=" + description; 
+}
+
+    @Override 
+     public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.description);
+        hash = 53 * hash + Objects.hashCode(this.accused);
+        return hash;
+}
+
+   @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DetectiveNotebook other = (DetectiveNotebook) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.accused, other.accused)) {
+            return false;
+        }
+        return true;
+    }
+    
+
+    
+    
     private void mustard() {
           System.out.println("Colonel Mustard is a dignified, dapper and "
                   + "\n dangerous military man. ");
@@ -96,9 +235,94 @@ public class ListSuspectsView extends View {
                 + "\n identity of Reverand Green. He has taken money from "
                 + "\n everyone from mobsters to businessmen.");
     }
+    
+    
+    private static Suspects[] createSuspects() throws ListSuspectsViewException {
+        // create and array(list of detective notebook items
+        
+        
+        ListSuspectsView[] suspect = new ListSuspectsView[6];
+        
+        ListSuspectsView mustard = new ListSuspectsView();
+        mustard.setDescription("Col. Mustard");
+        mustard.setAccused(true);
+        suspect[Suspects.suspectOne.ordinal()] = mustard;
+        
+        ListSuspectsView white = new ListSuspectsView();
+        white.setDescription("Mrs. White");
+        white.setAccused(false);
+        suspect[Suspects.suspectTwo.ordinal()] = white;
+        
+        ListSuspectsView plum = new ListSuspectsView();
+        plum.setDescription("Professor Plum");
+        plum.setAccused(true);
+        suspect[Suspects.suspectThree.ordinal()] = plum;
+        
+        ListSuspectsView peacock = new ListSuspectsView();
+        peacock.setDescription("Mrs. Peacock");
+        peacock.setAccused(false);
+        suspect[Suspects.suspectFour.ordinal()] = peacock;
+        
+        ListSuspectsView scarlet = new ListSuspectsView();
+        scarlet.setDescription("Mrs. Scarlet");
+        scarlet.setAccused(true);
+        suspect[Suspects.suspectFive.ordinal()] = scarlet;
+        
+        ListSuspectsView green = new ListSuspectsView();
+        green.setDescription("Mr. Green");
+        green.setAccused(false);
+        suspect[Suspects.suspectSix.ordinal()] = green;
+        
+        return null;
+        
+    }
 
-
-
-
+        public static void saveGame(Game game, String filepath) 
+        throws GameControlException {
+        
+    try( FileOutputStream fops = new FileOutputStream(filepath)) {
+        ObjectOutputStream output = new ObjectOutputStream(fops);
+        
+        output.writeObject(game);
+    }
+    catch(IOException ex) {
+        throw new GameControlException(ex.getMessage());
+    }
+    
+        }
 }
+   /* public static Suspects[] getSortedSuspects() throws GameControlException {
+        
+        // get inventory list for the current game
+       Suspects[] originalSuspects = 
+               suspects.getSuspect();
+        
+        // clone (make a copy) orignalList
+        Suspects[] suspectList = originalSuspects.clone();
+        
+       // using a BubbleSort to sort the list of inventoryList by name
+        Suspects tempSuspects;
+        for (int i = 0; i < suspectList.length-1; i++) {
+            for (int j = 0; j < suspectList.length-1-i; j++) {
+                if (suspectList[j].getDescription().
+                        compareToIgnoreCase(suspectList[j + 1].getDescription()) > 0) {
+                    tempSuspects = suspectList[j];
+                    suspectList[j] = suspectList[j+1];
+                    suspectList[j+1] = tempSuspects;
+                    throw new GameControlException("Can not display notebook"
+                                                  + " because error.");
+               }
+            }
+        }
+        
+        return suspectList;
+    }    
+
+ //public static Suspects[] getsuspect() {    
+   // return getSuspects;
+//}    */
+
+
+
+
     
